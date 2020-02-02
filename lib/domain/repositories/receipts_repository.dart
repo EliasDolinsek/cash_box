66277a7e44b079bbfc2df0b714cf0750aa959bf5
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-02-02 09:27:03 +0100

    Implemented remove-receipt_use_case

--- lib/domain/repositories/receipts_repository.dart
+++ lib/domain/repositories/receipts_repository.dart
@@ -1,6 +1,5 @@
 import 'package:cash_box/core/errors/failure.dart';
 import 'package:cash_box/domain/enteties/receipt.dart';
-import 'package:cash_box/domain/enteties/tag.dart';
 import 'package:cash_box/domain/repositories/empty_data.dart';
 import 'package:dartz/dartz.dart';
 
@@ -8,10 +7,6 @@ abstract class ReceiptsRepository {
 
   Future<Either<Failure, List<Receipt>>> getReceipts();
   Future<Either<Failure, Receipt>> getReceipt(String id);
-
   Future<Either<Failure, EmptyData>> updateReceipt(String id, Receipt receipt);
-
-  Future<Either<Failure, EmptyData>> addReceiptTag(String id, Tag tag);
-  Future<Either<Failure, EmptyData>> addReceiptField(String id, Tag tag);
-
+  Future<Either<Failure, EmptyData>> removeReceipt(String id);
 }
---
+++ test/domain/usecases/receipts/remove_receipt_use_case_test.dart
@@ -0,0 +1,31 @@
+import 'package:cash_box/domain/repositories/empty_data.dart';
+import 'package:cash_box/domain/repositories/receipts_repository.dart';
+import 'package:cash_box/domain/usecases/receipts/remove_receipt_use_case.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import '../../../fixtures/receipts_fixtures.dart' as receiptFixtures;
+
+class MockReceiptsRepository extends Mock implements ReceiptsRepository {}
+
+void main(){
+
+  MockReceiptsRepository repository;
+  RemoveReceiptUseCase useCase;
+
+  setUp((){
+    repository = MockReceiptsRepository();
+    useCase = RemoveReceiptUseCase(repository);
+  });
+
+  final testID = "abc-123";
+  test("should call the repository to remove the receipt", () async {
+    when(repository.removeReceipt(testID)).thenAnswer((_) async => Right(EmptyData()));
+    when(repository.getReceipt(testID)).thenAnswer((_) async => Right(receiptFixtures.receiptFixtures.first));
+    final params = RemoveReceiptUseCaseParams(testID);
+    final result = await useCase(params);
+
+    expect(result, Right(EmptyData()));
+    verify(repository.removeReceipt(testID));
+  });
+}
