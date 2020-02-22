d78fef75fb05258bbaaea1999ef775318747961c
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-02-22 11:36:17 +0100

    Implemented receipt_model

--- lib/data/models/receipt_model.dart
+++ lib/data/models/receipt_model.dart
@@ -1,8 +1,10 @@
+import 'package:cash_box/data/models/field_model.dart';
+import 'package:cash_box/data/models/model.dart';
 import 'package:cash_box/domain/enteties/field.dart';
 import 'package:cash_box/domain/enteties/receipt.dart';
 import 'package:meta/meta.dart';
 
-class ReceiptModel extends Receipt {
+class ReceiptModel extends Receipt implements Model {
 
   ReceiptModel(String id,
       {@required ReceiptType type,
@@ -16,4 +18,25 @@ class ReceiptModel extends Receipt {
           fields: fields,
           tagIDs: tagIDs,
         );
+
+  factory ReceiptModel.fromMap(Map<String, dynamic> map){
+    final type = ReceiptTypeConverter.fromString(map["type"]);
+    final creationDate = DateTime.fromMillisecondsSinceEpoch(map["creationDate"]);
+
+    final List fieldsAsMapList = map["fields"];
+    final fieldsList = fieldsAsMapList.map((fieldMap) => FieldModel.fromMap(fieldMap)).toList();
+    return ReceiptModel(map["id"], type: type, creationDate: creationDate, fields: fieldsList, tagIDs: map["tagIDs"]);
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    final fieldsAsMap = fields.map((field) => FieldModel.fromField(field).toMap()).toList();
+    return {
+      "id":id,
+      "type":type.asString,
+      "creationDate":creationDate.millisecondsSinceEpoch,
+      "fields":fieldsAsMap,
+      "tagIDs":tagIDs
+    };
+  }
 }
--- lib/domain/enteties/receipt.dart
+++ lib/domain/enteties/receipt.dart
@@ -13,13 +13,45 @@ class Receipt extends UniqueComponent {
       @required this.creationDate,
       @required this.fields,
       @required this.tagIDs})
-      : super(id, params: [type, creationDate.year, creationDate.month, creationDate.day, creationDate.hour, creationDate.minute, creationDate.second, fields, tagIDs]);
+      : super(id, params: [
+          type,
+          creationDate.year,
+          creationDate.month,
+          creationDate.day,
+          creationDate.hour,
+          creationDate.minute,
+          creationDate.second,
+          fields,
+          tagIDs
+        ]);
 
   @override
   String toString() {
     return 'Receipt{type: $type, creationDate: $creationDate, fields: $fields, tagIDs: $tagIDs}';
   }
-
 }
 
 enum ReceiptType { INCOME, OUTCOME, INVESTMENT, BANK_STATEMENT }
+
+extension ReceiptTypeConverter on ReceiptType {
+
+  String get asString {
+    switch(this){
+      case ReceiptType.INCOME: return "income";
+      case ReceiptType.OUTCOME: return "outcome";
+      case ReceiptType.INVESTMENT: return "investment";
+      case ReceiptType.BANK_STATEMENT: return "bank_statement";
+      default: throw new Exception("Couldn't convert ReceiptType: $this to string");
+    }
+  }
+
+  static ReceiptType fromString(String type){
+    switch(type){
+      case "income": return ReceiptType.INCOME;
+      case "outcome": return ReceiptType.OUTCOME;
+      case "investment": return ReceiptType.INVESTMENT;
+      case "bank_statement": return ReceiptType.BANK_STATEMENT;
+      default: throw new Exception("Couldn't resolve ReceiptType from String $type");
+    }
+  }
+}
