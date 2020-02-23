64fd6c9947d0bb83b46c85326116c182800e83a0
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-02-23 13:17:48 +0100

    Implemented tag_model

--- lib/data/models/tag_model.dart
+++ lib/data/models/tag_model.dart
@@ -1,10 +1,24 @@
+import 'package:cash_box/data/models/model.dart';
 import 'package:cash_box/domain/enteties/tag.dart';
 
 import 'package:meta/meta.dart';
 
-class TagModel extends Tag {
+class TagModel extends Tag implements Model {
 
   TagModel(String id, {@required String name, @required String color})
       : super(id, name: name, color: color);
+
+  factory TagModel.fromMap(Map<String, dynamic> map){
+    return TagModel(map["id"], name: map["name"], color: map["color"]);
+  }
+  
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "id":id,
+      "name":name,
+      "color":color,
+    };
+  }
 
 }
--- test/data/models/tag_model_test.dart
+++ test/data/models/tag_model_test.dart
@@ -1,11 +1,18 @@
+import 'package:cash_box/data/models/tag_model.dart';
 import 'package:cash_box/domain/enteties/tag.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main(){
 
-  final testTag = Tag("abc-123", name: "name", color: "color");
+  final testTag = TagModel("abc-123", name: "name", color: "color");
 
   test("should test if TagModel extends Tag", (){
     expect(testTag, isA<Tag>());
+  });
+
+  test("toMAP and fromMAP", (){
+    final map = testTag.toMap();
+    final tagModelFroMMap = TagModel.fromMap(map);
+    expect(testTag, tagModelFroMMap);
   });
 }
