a6e6123518ca018a2da550b066d8ed1a6a18ce8d
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-02-23 13:23:22 +0100

    Implemented template_model

--- lib/data/models/template_model.dart
+++ lib/data/models/template_model.dart
@@ -1,10 +1,28 @@
+import 'package:cash_box/data/models/field_model.dart';
+import 'package:cash_box/data/models/model.dart';
 import 'package:cash_box/domain/enteties/field.dart';
 import 'package:cash_box/domain/enteties/template.dart';
 import 'package:meta/meta.dart';
 
-class TemplateModel extends Template {
+class TemplateModel extends Template implements Model {
 
   TemplateModel(String id, {@required String name, @required List<Field> fields})
       : super(id, name: name, fields: fields);
+
+  factory TemplateModel.fromMap(Map<String, dynamic> map){
+    final List fieldsAsMapList = map["fields"];
+    final fieldsAsList = fieldsAsMapList.map((fieldMap) => FieldModel.fromMap(fieldMap)).toList();
+    return TemplateModel(map["id"], name: map["name"], fields: fieldsAsList);
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    final fieldsAsMap = fields.map((field) => FieldModel.fromField(field).toMap()).toList();
+    return {
+      "id":id,
+      "name":name,
+      "fields": fieldsAsMap
+    };
+  }
 
 }
--- test/data/models/template_model_test.dart
+++ test/data/models/template_model_test.dart
@@ -1,3 +1,5 @@
+import 'package:cash_box/data/models/field_model.dart';
+import 'package:cash_box/data/models/template_model.dart';
 import 'package:cash_box/domain/enteties/template.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -5,9 +7,15 @@ import '../../fixtures/field_fixtures.dart';
 
 void main(){
 
-  final testTemplate = Template("abc-123", name: "name", fields: fieldFixtures);
+  final testTemplate = TemplateModel("abc-123", name: "name", fields: fieldFixtures.map((field) => FieldModel.fromField(field)).toList());
 
   test("should test if TemplateModel extends Template", (){
     expect(testTemplate, isA<Template>());
+  });
+
+  test("toMap and fromMap", (){
+    final map = testTemplate.toMap();
+    final templateModelFromMap = TemplateModel.fromMap(map);
+    expect(templateModelFromMap, testTemplate);
   });
 }
