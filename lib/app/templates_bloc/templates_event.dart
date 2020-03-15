eeeff6c8c613b7e76538534c7556b62bd19aa229
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-03-15 16:42:10 +0100

    Implemented templates_state and templates_event

--- lib/app/templates_bloc/templates_event.dart
+++ lib/app/templates_bloc/templates_event.dart
@@ -1,5 +1,45 @@
+import 'package:cash_box/domain/core/enteties/fields/field.dart';
+import 'package:cash_box/domain/core/enteties/templates/template.dart';
 import 'package:equatable/equatable.dart';
 
-abstract class TemplatesEvent extends Equatable {
-  const TemplatesEvent();
+abstract class TemplatesEvent extends Equatable {}
+
+class AddTemplateEvent extends TemplatesEvent {
+  final Template template;
+
+  AddTemplateEvent(this.template);
+
+  @override
+  List get props => [template];
+}
+
+class GetTemplateEvent extends TemplatesEvent {
+  final String templateID;
+
+  GetTemplateEvent(this.templateID);
+
+  @override
+  List get props => [templateID];
+}
+
+class GetTemplatesEvent extends TemplatesEvent {}
+
+class RemoveTemplateEvent extends TemplatesEvent {
+  final String templateID;
+
+  RemoveTemplateEvent(this.templateID);
+
+  @override
+  List get props => [templateID];
+}
+
+class UpdateTemplateEvent extends TemplatesEvent {
+
+  final String id, name;
+  final List<Field> fields;
+
+  UpdateTemplateEvent({this.id, this.name, this.fields});
+
+  @override
+  List get props => [id, name, fields];
 }
--- lib/app/templates_bloc/templates_state.dart
+++ lib/app/templates_bloc/templates_state.dart
@@ -1,10 +1,38 @@
+import 'package:cash_box/domain/core/enteties/templates/template.dart';
 import 'package:equatable/equatable.dart';
 
-abstract class TemplatesState extends Equatable {
-  const TemplatesState();
-}
+abstract class TemplatesState extends Equatable {}
 
 class InitialTemplatesState extends TemplatesState {
   @override
   List<Object> get props => [];
+}
+
+class TemplatesAvailableState extends TemplatesState {
+  final List<Template> templates;
+
+  TemplatesAvailableState(this.templates);
+
+  @override
+  List get props => [templates];
+}
+
+class TemplatesUnavailableState extends TemplatesState {}
+
+class TemplatesErrorState extends TemplatesState {
+  final String errorMessage;
+
+  TemplatesErrorState(this.errorMessage);
+
+  @override
+  List get props => [errorMessage];
+}
+
+class TemplateAvailableState extends TemplatesState {
+  final Template template;
+
+  TemplateAvailableState(this.template);
+
+  @override
+  List get props => [template];
 }
