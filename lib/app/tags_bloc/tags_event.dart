855089b71076f278eabc5ef3c5a237ebef718f64
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-03-15 16:34:09 +0100

    Implemetned tags_Sate and tags_event

--- lib/app/tags_bloc/tags_event.dart
+++ lib/app/tags_bloc/tags_event.dart
@@ -1,5 +1,47 @@
+import 'package:cash_box/domain/core/enteties/tags/tag.dart';
 import 'package:equatable/equatable.dart';
 
-abstract class TagsEvent extends Equatable {
-  const TagsEvent();
+abstract class TagsEvent extends Equatable {}
+
+class AddTagEvent extends TagsEvent {
+
+  final Tag tag;
+
+  AddTagEvent(this.tag);
+
+  @override
+  List get props => [tag];
+}
+
+class GetTagEvent extends TagsEvent {
+  final String bucketID;
+
+  GetTagEvent(this.bucketID);
+
+  @override
+  List get props => [bucketID];
+}
+
+class GetTagsEvent extends TagsEvent {}
+
+class RemoveTagEvent extends TagsEvent {
+
+  final String bucketID;
+
+  RemoveTagEvent(this.bucketID);
+
+  @override
+  List get props => [bucketID];
+}
+
+class UpdateTagEvent extends TagsEvent {
+
+  final String id;
+  final String name, description;
+  final List<String> receiptIDs;
+
+  UpdateTagEvent({this.id, this.name, this.description, this.receiptIDs});
+
+  @override
+  List get props => [id, name, description, receiptIDs];
 }
--- lib/app/tags_bloc/tags_state.dart
+++ lib/app/tags_bloc/tags_state.dart
@@ -1,10 +1,43 @@
+import 'package:cash_box/domain/core/enteties/tags/tag.dart';
 import 'package:equatable/equatable.dart';
 
-abstract class TagsState extends Equatable {
-  const TagsState();
-}
+abstract class TagsState extends Equatable {}
 
 class InitialTagsState extends TagsState {
   @override
   List<Object> get props => [];
+}
+
+class TagsAvailableState extends TagsState {
+
+  final List<Tag> tags;
+
+  TagsAvailableState(this.tags);
+
+  @override
+  List get props => [tags];
+
+}
+
+class TagsUnavailableState extends TagsState {}
+
+class TagsErrorState extends TagsState {
+
+  final String errorMessage;
+
+  TagsErrorState(this.errorMessage);
+
+  @override
+  List get props => [errorMessage];
+}
+
+class TagAvailableState extends TagsState {
+
+  final Tag tag;
+
+  TagAvailableState(this.tag);
+
+  @override
+  List get props => [tag];
+
 }
