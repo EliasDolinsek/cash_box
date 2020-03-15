2eafc1c803a6dcbba1e2a1756ee158b0135b63f9
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-03-15 14:40:33 +0100

    Implemented buckets_state and buckets_event

--- lib/app/buckets_bloc/buckets_event.dart
+++ lib/app/buckets_bloc/buckets_event.dart
@@ -1,5 +1,65 @@
+import 'package:cash_box/domain/core/enteties/buckets/bucket.dart';
 import 'package:equatable/equatable.dart';
 
-abstract class BucketsEvent extends Equatable {
-  const BucketsEvent();
+abstract class BucketsEvent extends Equatable {}
+
+class AddBucketEvent extends BucketsEvent {
+  final Bucket bucket;
+
+  AddBucketEvent(this.bucket);
+
+  @override
+  List get props => [bucket];
+}
+
+class AddReceiptToBucketEvent extends BucketsEvent {
+  final String bucketID, receiptID;
+
+  AddReceiptToBucketEvent(this.bucketID, this.receiptID);
+
+  @override
+  List get props => [bucketID, receiptID];
+}
+
+class GetBucketEvent extends BucketsEvent {
+  final String bucketID;
+
+  GetBucketEvent(this.bucketID);
+
+  @override
+  List get props => [bucketID];
+}
+
+class GetBucketsEvent extends BucketsEvent {}
+
+class RemoveBucketEvent extends BucketsEvent {
+
+  final String bucketID;
+
+  RemoveBucketEvent(this.bucketID);
+
+  @override
+  List get props => [bucketID];
+}
+
+class RemoveReceiptFromBucketEvent extends BucketsEvent {
+
+  final String receiptID, bucketID;
+
+  RemoveReceiptFromBucketEvent(this.receiptID, this.bucketID);
+
+  @override
+  List get props => [receiptID, bucketID];
+}
+
+class UpdateBucketEvent extends BucketsEvent {
+
+  final String id;
+  final String name, description;
+  final List<String> receiptIDs;
+
+  UpdateBucketEvent({this.id, this.name, this.description, this.receiptIDs});
+
+  @override
+  List get props => [id, name, description, receiptIDs];
 }
--- lib/app/buckets_bloc/buckets_state.dart
+++ lib/app/buckets_bloc/buckets_state.dart
@@ -1,10 +1,32 @@
+import 'package:cash_box/domain/core/enteties/buckets/bucket.dart';
 import 'package:equatable/equatable.dart';
 
-abstract class BucketsState extends Equatable {
-  const BucketsState();
-}
+abstract class BucketsState extends Equatable {}
 
 class InitialBucketsState extends BucketsState {
   @override
   List<Object> get props => [];
+}
+
+class BucketsAvailableState extends BucketsState {
+
+  final List<Bucket> buckets;
+
+  BucketsAvailableState(this.buckets);
+
+  @override
+  List get props => [buckets];
+
+}
+
+class BucketsUnavailableState extends BucketsState {}
+
+class BucketsErrorState extends BucketsState {}
+
+class BucketAvailableState extends BucketsState {
+
+  final Bucket bucket;
+
+  BucketAvailableState(this.bucket);
+
 }
