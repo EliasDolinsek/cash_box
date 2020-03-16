e8a1883c3b016733d98c7ac8ff1a126b88d59b2f
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-03-16 10:51:11 +0100

    Implemented contacts_bloc

--- test/app/contacts_bloc/contacts_bloc_test.dart
+++ test/app/contacts_bloc/contacts_bloc_test.dart
@@ -1,33 +1,102 @@
-
 import 'package:cash_box/app/contacts_bloc/bloc.dart';
+import 'package:cash_box/core/usecases/use_case.dart';
+import 'package:cash_box/domain/core/enteties/contacts/contact.dart';
+import 'package:cash_box/domain/core/repositories/empty_data.dart';
 import 'package:cash_box/domain/core/usecases/contacts/add_contact_use_case.dart';
 import 'package:cash_box/domain/core/usecases/contacts/get_contact_use_case.dart';
+import 'package:cash_box/domain/core/usecases/contacts/get_contacts_use_case.dart';
 import 'package:cash_box/domain/core/usecases/contacts/remove_contact_use_case.dart';
 import 'package:cash_box/domain/core/usecases/contacts/update_contact_use_case.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+
+import '../../fixtures/contact_fixtures.dart';
+import '../../fixtures/field_fixtures.dart';
 
 class MockAddContactUseCase extends Mock implements AddContactUseCase {}
 
 class MockGetContactUseCase extends Mock implements GetContactUseCase {}
 
-class MockGetContactsUseCase extends Mock implements GetContactUseCase {}
+class MockGetContactsUseCase extends Mock implements GetContactsUseCase {}
 
 class MockRemoveContactUseCase extends Mock implements RemoveContactUseCase {}
 
 class MockUpdateContactUseCase extends Mock implements UpdateContactUseCase {}
 
-void main(){
-
+void main() {
   final MockAddContactUseCase addContactUseCase = MockAddContactUseCase();
   final MockGetContactUseCase getContactUseCase = MockGetContactUseCase();
   final MockGetContactsUseCase getContactsUseCase = MockGetContactsUseCase();
-  final MockRemoveContactUseCase removeContactUseCase = MockRemoveContactUseCase();
-  final MockUpdateContactUseCase updateContactUseCase = MockUpdateContactUseCase();
-  
+  final MockRemoveContactUseCase removeContactUseCase =
+      MockRemoveContactUseCase();
+  final MockUpdateContactUseCase updateContactUseCase =
+      MockUpdateContactUseCase();
+
   ContactsBloc contactsBloc;
-  
-  setUp((){
-    contactsBloc = ContactsBloc();
+
+  setUp(() {
+    contactsBloc = ContactsBloc(
+        addContactUseCase: addContactUseCase,
+        getContactUseCase: getContactUseCase,
+        getContactsUseCase: getContactsUseCase,
+        removeContactUseCase: removeContactUseCase,
+        updateContactUseCase: updateContactUseCase);
+  });
+
+  test("AddContactEvent", () async {
+    final contact = contactFixtures.first;
+    final params = AddContactParams(contact);
+    when(addContactUseCase.call(params))
+        .thenAnswer((_) async => Right(EmptyData()));
+
+    final expected = [InitialContactsState()];
+
+    expectLater(contactsBloc.state, emitsInOrder(expected));
+
+    final event = AddContactEvent(contact);
+    contactsBloc.dispatch(event);
+  });
+
+  test("GetContactEvent", () async {
+    final contact = contactFixtures.first;
+    final params = GetContactUseCaseParams(contact.id);
+    when(getContactUseCase.call(params))
+        .thenAnswer((_) async => Right(contact));
+
+    final expected = [InitialContactsState(), ContactAvailableState(contact)];
+
+    expectLater(contactsBloc.state, emitsInOrder(expected));
+
+    final event = GetContactEvent(contact.id);
+    contactsBloc.dispatch(event);
+  });
+
+  test("GetContactsEvent", () async {
+    when(getContactsUseCase.call(NoParams()))
+        .thenAnswer((_) async => Right(contactFixtures));
+
+    final expected = [
+      InitialContactsState(),
+      ContactsAvailableState(contactFixtures)
+    ];
+
+    expectLater(contactsBloc.state, emitsInOrder(expected));
+
+    final event = GetContactsEvent();
+    contactsBloc.dispatch(event);
+  });
+
+  test("UpdateContactEvent", () async {
+    final contact = contactFixtures.first;
+    final update = Contact(contact.id, fields: fieldFixtures);
+
+    final params = UpdateContactUseCaseParams(update.id, update.fields);
+    when(updateContactUseCase.call(params)).thenAnswer((_) async => Right(EmptyData()));
+
+    final expected = [InitialContactsState()];
+    expectLater(contactsBloc.state, emitsInOrder(expected));
+
+    contactsBloc.dispatch(GetContactsEvent());
   });
 }
