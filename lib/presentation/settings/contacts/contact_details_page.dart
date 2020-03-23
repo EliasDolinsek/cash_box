8e92c3f2c95013a5b5f6d8f2aac472882a0ca721
Elias Dolinsek <elias.dolinsek@gmail.com>
2020-03-23 15:54:11 +0100

    Implemented delete-confirmation-dialog for contacts_details_page

--- lib/presentation/settings/contacts/contact_details_page.dart
+++ lib/presentation/settings/contacts/contact_details_page.dart
@@ -4,6 +4,7 @@ import 'package:cash_box/domain/core/enteties/contacts/contact.dart';
 import 'package:cash_box/domain/core/enteties/fields/field.dart';
 import 'package:cash_box/localizations/app_localizations.dart';
 import 'package:cash_box/presentation/fields/field_card_widget.dart';
+import 'package:cash_box/presentation/settings/dialogs/delete_dialog.dart';
 import 'package:flutter/material.dart';
 
 class ContactDetailsPage extends StatefulWidget {
@@ -16,6 +17,8 @@ class ContactDetailsPage extends StatefulWidget {
 }
 
 class _ContactDetailsPageState extends State<ContactDetailsPage> {
+
+  bool _delete = false;
   String _name;
   List _fields;
 
@@ -28,12 +31,13 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
 
   @override
   void dispose() {
-    _updateContact();
+   if(!_delete) _updateContact();
     super.dispose();
   }
 
-  void _updateContact(){
-    final event = UpdateContactEvent(widget.contact.id, name: _name, fields: _fields);
+  void _updateContact() {
+    final event = UpdateContactEvent(
+        widget.contact.id, name: _name, fields: _fields);
     sl<ContactsBloc>().dispatch(event);
   }
 
@@ -48,11 +52,19 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
       body: _buildListView(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
+        onPressed: _addEmptyField,
         label: Text(AppLocalizations.translateOf(context, "btn_add_field")),
         icon: Icon(Icons.add),
       ),
     );
+  }
+
+  void _addEmptyField() {
+    setState(() {
+      final field = Field.newField(
+          type: FieldType.text, description: "", value: "");
+      _fields.add(field);
+    });
   }
 
   Widget _buildListView() {
@@ -74,13 +86,13 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
 
   Widget _buildNameFieldCardWidget() {
     final field =
-        Field.newField(type: FieldType.text, description: "Name", value: _name);
+    Field.newField(type: FieldType.text, description: "Name", value: _name);
     return FieldCard(
       field,
       deletable: false,
       descriptionEditable: false,
       typeEditable: false,
-      onFieldChanged: (Field field){
+      onFieldChanged: (Field field) {
         _name = field.value;
       },
     );
@@ -92,11 +104,12 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
         field,
         key: ValueKey(field),
         onFieldChanged: (update) {
-          final index = _fields.indexWhere((element) => element.id == update.id);
+          final index = _fields.indexWhere((element) =>
+          element.id == update.id);
           _fields.removeAt(index);
           _fields.insert(index, update);
         },
-        onDelete: (){
+        onDelete: () {
           setState(() {
             _fields.remove(field);
           });
@@ -119,13 +132,22 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
         Icons.delete,
         color: Colors.red,
       ),
-      onPressed: () => _removeContact(context),
+      onPressed: _showDeleteConfirmationDialog,
     );
+  }
+
+  void _showDeleteConfirmationDialog() async {
+    final result = await showDialog(context: context, builder: (_) => DeleteDialog());
+    if(result != null && result){
+      _removeContact(context);
+    }
   }
 
   void _removeContact(BuildContext context) {
     final event = RemoveContactEvent(widget.contact.id);
     sl<ContactsBloc>().dispatch(event);
+
+    _delete = true;
     Navigator.pop(context);
   }
 }
